c86edffa9f4557949f1fbd836e133b9af381d616
Fateme Emadi <fatemehemadi2021@gmail.com>
2022-02-12 16:32:21 +0330

    sql

---
+++ insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers(name,city,country) VALUES
+('zahra','mashhad','iran'),('shima','mashhad','iran'),('ali','shiraz','iran'),('nima','tehran','iran'),('amir','ankara','turkey')
+
+INSERT INTO products(name,count,price) VALUES
+('macbook',2,50000000),('iphone 11 pro',4,30000000),('galaxy s20',3,20000000),('ipad',0,40000000)
---
+++ select3.sql
@@ -0,0 +1,1 @@
+SELECT * FROM products where NOT price BETWEEN 20000000 AND 30000000
